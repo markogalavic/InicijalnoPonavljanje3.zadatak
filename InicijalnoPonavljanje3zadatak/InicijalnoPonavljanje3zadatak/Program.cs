d1b543ecbd9da8836e8bd9901115af5a540f5f44
markogalavic <55134792+markogalavic@users.noreply.github.com>
2020-09-12 15:04:50 +0200

    Add files via upload

---
+++ InicijalnoPonavljanje3zadatak/InicijalnoPonavljanje3zadatak/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje3zadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Unesi jedan broj");
+            int broj = Convert.ToInt32(Console.ReadLine());
+
+            if (broj % 4 == 0)
+            {
+                Console.WriteLine("Broj je dijeljiv sa cetiri");
+            }
+            else if (broj % 6 == 0)
+            {
+                Console.WriteLine("Broj je dijeljiv sa sest");
+            }
+            else if (broj % 4 == 0|| broj % 6 == 0)
+            {
+                Console.WriteLine("Broj je dijeljiv sa cetiri i sa sest");
+            }
+            Console.ReadKey();
+        }
+    }
+}
